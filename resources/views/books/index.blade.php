e0489802e14fcfab7a9dc65e21f858860010271e
djabirpc <djabir.pc@outlook.fr>
2024-03-20 15:05:34 +0100

    Ep 15

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,7 +2,32 @@
 
 @section('content')
   <h1 class="mb-10 text-2x1">Books</h1>
-  <form action=""></form>
+
+  <form action="{{route('books.index')}}" method='GET' class="mb-4 flex items-center space-x-2">
+    <input type="text" name="title" placeholder="Search by Title"
+      value="{{request('title')}}" class="input h-10" />
+    <input type="hidden" name="filter" value="{{ request('filter') }}" />
+    <button type="submit" class="btn h-10">Search</button>
+    <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
+  </form>
+  <div class="filter-container mb-4 flex">
+    @php
+      $filters = [
+          '' => 'Latest',
+          'popular_last_month' => 'Popular Last Month',
+          'popular_last_6months' => 'Popular Last 6 Months',
+          'highest_rated_last_month' => 'Highest Rated Last Month',
+          'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+      ];
+    @endphp
+
+    @foreach ($filters as $key => $label)
+      <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+        class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+        {{ $label }}
+      </a>
+    @endforeach
+  </div>
   <ul>
     @forelse($books as $book)
     <li class="mb-4">
@@ -28,7 +53,7 @@
     <li class="mb-4">
       <div class="empty-book-item">
         <p class="empty-text">No books found</p>
-        <a href="{{ route('books.index', $book)}}" class="reset-link">Reset criteria</a>
+        <a href="{{ route('books.index')}}" class="reset-link">Reset criteria</a>
       </div>
     </li>
     @endforelse
